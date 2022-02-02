05f796fc0402bb3ddc09d73f4aeb11627c056b34
Thiago Carvalho <thiago.xsource@gmail.com>
2022-02-02 12:25:33 -0300

    :tada: convert to ts dashboard and header

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -2,13 +2,13 @@ import { useEffect, useState } from 'react';
 
 import { Header } from '../../components/Header';
 import api from '../../services/api';
-import Food from '../../components/Food';
+import {Food} from '../../components/Food';
 import ModalAddFood from '../../components/ModalAddFood';
 import ModalEditFood from '../../components/ModalEditFood';
 import { FoodsContainer } from './styles';
 
 type IFood = {
-    id?: number,
+    id: number,
     name: string,
     description: string,
     price: number,
